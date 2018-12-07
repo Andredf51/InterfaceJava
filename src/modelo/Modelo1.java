3054b96a976db95e98f3101b1a00a455a9c66a75
Andredf51 <andrels17@outlook.com>
2018-12-07 20:34:51 -0200

    Nova interface

---
+++ src/modelo/Modelo1.java
@@ -0,0 +1,26 @@
+/*
+Criado para interligar as 2 frames
+ */
+package modelo;
+
+/**
+ *
+ * @author andre
+ */
+public class Modelo1 {
+    private String nome;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
